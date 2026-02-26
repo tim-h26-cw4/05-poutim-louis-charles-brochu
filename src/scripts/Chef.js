8ed71ffff1d17fa85582deda9c26b2fff910620a
Louis-Charles <louischarles.brochu@gmail.com>
2026-02-26 15:54:58 -0500

    Fix dernier bug et optimisation

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -1,18 +1,14 @@
 import Poutine from './Poutine.js';
 export default class Chef {
   constructor(element) {
-    this.container = document.querySelector('.chef__order');
-    this.commander = document.querySelectorAll('.js-commande');
+    this.container = element.querySelector('.chef__order');
+    this.commander = element.querySelector('.js-commande');
     this.element = element;
     this.init();
   }
 
   init() {
-    console.log('Chef');
-    for (let i = 0; i < this.commander.length; i++) {
-      const button = this.commander[i];
-      button.addEventListener('click', this.sendOrder.bind(this));
-    }
+    this.commander.addEventListener('click', this.sendOrder.bind(this));
 
     const poutine = document.querySelectorAll('.js-poutine');
     for (let i = 0; i < poutine.length; i++) {
@@ -34,6 +30,6 @@ export default class Chef {
     this.container.innerHTML = '';
     const commande = document.createElement('p');
     commande.textContent = `total de poutines(s): ${count}`;
-    commande.appendChild(this.container);
+    this.container.appendChild(commande);
   }
 }
